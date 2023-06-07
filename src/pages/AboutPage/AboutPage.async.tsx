1d3c521aa04dc08c1012a21ccbe28407230a750c
Maksim Laurouski <maximlavrovsky@mail.ru>
2023-06-07 21:08:59 +0300

    feat: добавлен Роутинг и Lazy Suspence

---
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -0,0 +1,9 @@
+import {lazy} from 'react';
+
+export const  AboutPageAsync = lazy(()=>new Promise(res => {
+    setTimeout(()=>{
+        //@ts-ignore
+        res(import('./AboutPage'))
+        }, 1500
+    )
+}))
