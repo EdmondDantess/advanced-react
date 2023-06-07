94af5931a2234579350acd39eb12065944cb18d0
Maksim Laurouski <maximlavrovsky@mail.ru>
2023-06-07 20:14:55 +0300

    feat: настройка css modules

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
-import './Counter.scss'
+import classes from './Counter.module.scss'
 
 export const Counter = () => {
     const [count, setCount] = useState(0)
 
     return (
-        <div>
+        <div className={classes.counter}>
             <div>{count}</div>
             <button onClick={() => setCount(count + 1)}>increment</button>
         </div>
